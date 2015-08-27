fd830c4f92fe30a1f66de1d4049757046ed171e4
Daniel Plácido <daniel.uramg@gmail.com>
2015-08-27 13:38:04 -0300

    v034
    agora quando tem perguntas para responder toca um alerta ao invés de
    redicerionar para a página de perguntas
    Todos console.log() fora trocados por GM_log()

--- mercadofacil.js
+++ mercadofacil.js
@@ -4,7 +4,7 @@
 // @description	Modificações na página do ML para facilitar o gerenciamento das vendas
 // @author	Daniel Plácido (daniel.uramg@gmail.com)
 // @contributor	Marco Silveira (vastar@globo.com)
-// @version	0.33
+// @version	0.34
 // @downloadURL	https://raw.githubusercontent.com/danieluramg/MercadoFacil/master/mercadofacil.js
 // @updateURL	https://raw.githubusercontent.com/danieluramg/MercadoFacil/master/mercadofacil.js
 // @require	http://ideias.2p.fm/userscripts/jquery-2.1.1.min.js
@@ -12,6 +12,9 @@
 // @match	http://*.mercadolivre.com.br/*
 // @match	https://*.mercadolivre.com.br/*
 // @match	https://chat.mercadolibre.com/*
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_log
 // @run-at	document-end
 // ==/UserScript==
 
@@ -49,31 +52,29 @@ $(document).ready(function(){
 
     // VERIFICA PERGUNTAS PENDENTES //
     function verifica_perguntas(){
-        contador_perguntas = $('.ml-count').html(); //isto na página de perguntas
+        contador_perguntas = $('.ml-count').html();
         if (contador_perguntas >= 1){
-            console.log("Tem perguntas para responder!");
-            if (location.host != 'questions.mercadolivre.com.br') location.href='https://questions.mercadolivre.com.br/seller';
+            GM_log("Tem perguntas para responder!");
+            document.getElementById("alert").innerHTML="<embed src='https://www.webtrackerencomendas.com.br/alert.mp3' hidden=true autostart=true loop=false>";
         }
     }
     function loop_perguntas(){
         setTimeout(function(){
-            console.log("verificou perguntas");
+            GM_log("verificou perguntas");
             verifica_perguntas();
             loop_perguntas();
         },30000);
     }
-    if (mf_perguntas == 'checked') loop_perguntas();
+    if (mf_perguntas == 'checked'){
+        $('body').after('<div id="alert"></div>');
+        loop_perguntas();
+    }
     // VERIFICA PERGUNTAS PENDENTES //
 
     //  PAGINA DE AJUDA E CONTATO //
     if ( location.href == 'http://contato.mercadolivre.com.br/ajuda' ){
         // testa para saber se ja foi registrado o userName do usuário
         if (GM_getValue("mercadoFacil_userName")==null){
-
-            /////////////////////////////         temporariamente desativado para experimentar novo método         /////////////////////////////
-            // avisa que tera que ir a pagina de resumo para registrar o userName
-            //alert("Para que o chat seja mostrado nesta tela você tera que estar logado e visitar a página de resumo para que seu USERID seja registrado.\nVocê será redirecionado para a página de Resumo.");
-            //location.href="https://myaccount.mercadolivre.com.br/summary";
 
             //captura username e armazena
             user_name = $('label[for="nav-header-user-switch"]').html().replace('<!--[if lt IE 9]><a href="https://myaccount.mercadolivre.com.br/summary/" rel="nofollow"><![endif]-->', '').replace('<!--[if lt IE 9]></a><![endif]-->', '').replace(' <i class="nav-icon-user"></i>', '').replace(/^\s+|\s+$/g,"");
@@ -119,11 +120,6 @@ $(document).ready(function(){
         user_name = $('#nickName').html(); //capturar nome de usuario
         user_id = userId; //capturar id do usuario
 
-        /////////////////////////////         temporariamente desativado para experimentar novo método         /////////////////////////////
-        // registra o userName e o userID para ser acessado futuramente
-        //GM_setValue ("mercadoFacil_userName", user_name);
-        //GM_setValue ("mercadoFacil_userID", user_id);
-
         chat_button = '<div id="chat_container" class="chat-container"> <a id="null" href="javascript://chat" onclick="runChat();return false;" class="null" style="null"><span id="balloon" style="padding-left: 20px;background: url(\'https://secure.mlstatic.com/org-img/CHAT/icono_chat2.gif\') no-repeat scroll 0% 0% transparent;"> Iniciar chat </span></a><script type="text/javascript">function runChat() {if (typeof (window.chatWindow) == \'undefined\' || window.chatWindow.closed){var winOpts = \'directories=no,titlebar=no,toolbar=no,menubar=no,location=no,resizable=no,scrollbars=no,status=no,width=633,height=550\';var winName = \'Chat\';var winURL = \'https://chat.mercadolibre.com/\';window.chatWindow=window.open(\'about:blank\',winName,winOpts);var webchatForm = document.createElement(\'form\');webchatForm.setAttribute(\'action\', winURL);webchatForm.style.display=\'none\';webchatForm.setAttribute(\'target\',winName);webchatForm.setAttribute(\'method\',\'post\');var e = document.createElement(\'input\');e.setAttribute(\'name\',\'userData\');e.setAttribute(\'value\',JSON.stringify({"validate":true,"render":true,"agents_online":true,"available_agents":"","available_place_in_queue":"","allow_render":true,"user":{"user_id":"'+user_id+'","nickname":"'+user_name+'","first_name":"'+user_name+'","last_name":"","site":"MLB","user_type":"MERCADO_LIDER","segment":"LONG_TAIL","queue_id":"MEJORES_VENDEDORES_MLB"},"from_plugin":true,"group":"ML_PORTAL_HOME","idx":2,"article_number":"","articleDescription":"HomeNewPortal","sf":{"from_nw_mlportal__c":true},"lang":"pt_BR"}));webchatForm.appendChild(e);document.body.appendChild(webchatForm);webchatForm.submit();setTimeout(function(){webchatForm.parentNode.removeChild(webchatForm);},1000);}window.chatWindow.focus();}</script></div>';
         $('#chat_container').remove(); //remove a div do chat
         $('.myml-title').after(chat_button); //insere a div do chat com a função de chamada
@@ -159,7 +155,7 @@ $(document).ready(function(){
     function mfacil_config(){
         //injeta mascara de fundo e div com o formulário
         $('body').append('<div id="mfacil_fundo" style="  background-color: rgba(0,0,0,0.5);width: 100%;height: 100%;position: fixed;left: 0;top: 0;z-index: 1011;"></div>');
-        html_form_mfacil = '<div id="div_mfacil" style="z-index: 1011;background-color: #FFFFFF"> <a class="ch-close" onclick="$(\'#div_mfacil, #mfacil_fundo\').remove();" style="z-index:1010"></a> <h1 class="main-title title title--primary">MercadoFacil</h1> <input type="hidden" id="mfacil_first_install" value="instalado"> <table> <tbody> <tr title="Quando ativo, ao clicar no campo para responder uma pergunta, preenche automaticamente Bom dia/tarde/noite de acordo com o horário"> <td>Ativar/Desativar cumprimento automático</td> <td> &nbsp;<input type="checkbox" id="mfacil_cumprimento" ' + mf_cumprimento + '></td> </tr> <tr title="Remover banners de publicidade da página de Resumo"> <td>Remover banners de publicidade da página de Resumo </td> <td> &nbsp;<input type="checkbox" id="mfacil_banners" ' + mf_banners + '></td> </tr> <tr title="Tempo em Milisegundos para o chat ser atualizdo automaticamente quando não tiver atendente disponível"> <td>Tempo para atualizar o Chat</td> <td> &nbsp;<input type="text" id="mfacil_delay_chat" value="' + mf_delay_chat + '" size="5"> ms (padrão 1000ms {1 segundo})</td> </tr> <tr title="Ajusta a altura da Textarea de criação de anuncios"> <td>Altura da Textarea de criação de anúncios</td> <td> &nbsp;<input type="text" id="mfacil_textarea" value="' + mf_textarea + '" size="5"> px (padrão 560px)</td> </tr> <tr title="Quando ativo, a cada 30 segundos ser verificado se existem perguntas pendentes, e quando identificar redireciona para a página para responder"> <td>Ativar/Desativar verificação de perguntas pendentes</td> <td> &nbsp;<input type="checkbox" id="mfacil_perguntas" ' + mf_perguntas + '></td> </tr> <tr title="Quando ativo, você clica com o botão direito no campo para responder alguma pergunta e escolhe uma de suas respostas prontas para preenchimento automático"> <td>Ativar/Desativar Respostas prontas</td> <td> &nbsp;<input type="checkbox" id="mfacil_respostas" ' + mf_respostas + '></td> </tr>     </tbody>     </table>         <div id="respostas_prontas" style="display: none;">     <table> <thead> <tr><th>Título</th> <th>Resposta</th></tr> </thead> <tbody> <tr> <td> <input type="text" id="mfacil_tit1" value="' + mf_t1 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res1" value="' + mf_r1 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit2" value="' + mf_t2 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res2" value="' + mf_r2 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit3" value="' + mf_t3 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res3" value="' + mf_r3 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit4" value="' + mf_t4 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res4" value="' + mf_r4 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit5" value="' + mf_t5 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res5" value="' + mf_r5 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit6" value="' + mf_t6 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res6" value="' + mf_r6 + '" size="50"> </td> </tr> </tbody>  </table>         </div>     <button id="mfacil_salvar" class="ch-btn">Salvar</button> </div>';
+        html_form_mfacil = '<div id="div_mfacil" style="z-index: 1011;background-color: #FFFFFF"> <a class="ch-close" onclick="$(\'#div_mfacil, #mfacil_fundo\').remove();" style="z-index:1010"></a> <h1 class="main-title title title--primary">MercadoFacil</h1> <input type="hidden" id="mfacil_first_install" value="instalado"> <table> <tbody> <tr title="Quando ativo, ao clicar no campo para responder uma pergunta, preenche automaticamente Bom dia/tarde/noite de acordo com o horário"> <td>Ativar/Desativar cumprimento automático</td> <td> &nbsp;<input type="checkbox" id="mfacil_cumprimento" ' + mf_cumprimento + '></td> </tr> <tr title="Remover banners de publicidade da página de Resumo"> <td>Remover banners de publicidade da página de Resumo </td> <td> &nbsp;<input type="checkbox" id="mfacil_banners" ' + mf_banners + '></td> </tr> <tr title="Tempo em Milisegundos para o chat ser atualizdo automaticamente quando não tiver atendente disponível"> <td>Tempo para atualizar o Chat</td> <td> &nbsp;<input type="text" id="mfacil_delay_chat" value="' + mf_delay_chat + '" size="5"> ms (padrão 1000ms {1 segundo})</td> </tr> <tr title="Ajusta a altura da Textarea de criação de anuncios"> <td>Altura da Textarea de criação de anúncios</td> <td> &nbsp;<input type="text" id="mfacil_textarea" value="' + mf_textarea + '" size="5"> px (padrão 560px)</td> </tr> <tr title="Quando ativo, a cada 30 segundos ser verificado se existem perguntas pendentes, e quando identificar toca um som de alerta"> <td>Ativar/Desativar verificação de perguntas pendentes</td> <td> &nbsp;<input type="checkbox" id="mfacil_perguntas" ' + mf_perguntas + '></td> </tr> <tr title="Quando ativo, você clica com o botão direito no campo para responder alguma pergunta e escolhe uma de suas respostas prontas para preenchimento automático"> <td>Ativar/Desativar Respostas prontas</td> <td> &nbsp;<input type="checkbox" id="mfacil_respostas" ' + mf_respostas + '></td> </tr>     </tbody>     </table>         <div id="respostas_prontas" style="display: none;">     <table> <thead> <tr><th>Título</th> <th>Resposta</th></tr> </thead> <tbody> <tr> <td> <input type="text" id="mfacil_tit1" value="' + mf_t1 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res1" value="' + mf_r1 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit2" value="' + mf_t2 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res2" value="' + mf_r2 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit3" value="' + mf_t3 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res3" value="' + mf_r3 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit4" value="' + mf_t4 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res4" value="' + mf_r4 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit5" value="' + mf_t5 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res5" value="' + mf_r5 + '" size="50"> </td> </tr> <tr> <td> <input type="text" id="mfacil_tit6" value="' + mf_t6 + '" size="15"> </td> <td> &nbsp;<input type="text" id="mfacil_res6" value="' + mf_r6 + '" size="50"> </td> </tr> </tbody>  </table>         </div>     <button id="mfacil_salvar" class="ch-btn">Salvar</button> </div>';
         $('#mfacil_fundo').after(html_form_mfacil);
         if (mf_respostas == 'checked') $('#respostas_prontas').attr('style', 'display: block;');
     }
@@ -230,17 +226,16 @@ $(document).ready(function(){
     })
     // SALVAR FORM DO MERCADOFACIL //
 
-
     /******************** NA PAGINA DE PERGUNTAS ********************/
     if ( location.host == 'questions.mercadolivre.com.br' ) {
-        console.log("Funções da pagina de perguntas"); //debug
+        GM_log("Funções da pagina de perguntas"); //debug
 
         //preencher boas vindas no campo de responder perguntas
         if (mf_cumprimento == 'checked'){
             setTimeout(function(){ //da um delay pra carregar as perguntas
-                console.log("entrou na função do delay"); //debug
+                GM_log("entrou na função do delay"); //debug
                 $('textarea').click(function(e){
-                    console.log(e.target + " clicado"); //debug
+                    GM_log(e.target + " clicado"); //debug
                     if (! $(e.target).attr('comp') && $(e.target).attr('name') == 'Answer' ){ //se existir a variaves as boas-vindas nao serao preenchidas
                         $(e.target).val(hello() + ", ");
                         $(e.target).attr('comp', '1'); //atributo para so preencher as boas vindas uma vez
