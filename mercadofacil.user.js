c2037c437494ec69feaace45a9a95d409d7e0c89
danieluramg <daniel.uramg@gmail.com>
2015-10-19 12:23:11 -0200

    Update mercadofacil.user.js

--- mercadofacil.user.js
+++ mercadofacil.user.js
@@ -4,7 +4,7 @@
 // @description	Modificações na página do ML para facilitar o gerenciamento das vendas
 // @author	Daniel Plácido (daniel.uramg@gmail.com)
 // @contributor	Marco Silveira (vastar@globo.com)
-// @version	0.41
+// @version	0.42
 // @downloadURL	https://raw.githubusercontent.com/danieluramg/MercadoFacil/master/mercadofacil.user.js
 // @updateURL	https://raw.githubusercontent.com/danieluramg/MercadoFacil/master/mercadofacil.user.js
 // @require	http://ideias.2p.fm/userscripts/jquery-2.1.1.min.js
